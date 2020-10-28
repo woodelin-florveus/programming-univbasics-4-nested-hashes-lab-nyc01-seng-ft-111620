a7d1b055ec95ae362411eda7d2212395b78d9205
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-28 11:37:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,9 @@ def adding_matz
       }
     }
 
-		programmer_hash[:yukihiro_matsumoto]
+		programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
+
 
 end
 
