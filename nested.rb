99de234c79ac6fa7536156725d93b7541cda5273
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-28 11:43:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,7 +137,7 @@ def adding_to_dennis
       }
     }
 
-		programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+		programmer_hash[:dennis_ritchie][:languages] = ["Assembly"]
 		programmer_hash
 
 end
